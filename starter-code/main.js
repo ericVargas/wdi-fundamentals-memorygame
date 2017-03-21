323e904b041150bde7c118781f2dfeff1b304b2c
ericVargas <batman@erics-mbp.wifi.lagcc>
2017-03-21 15:22:23 -0400

    add an alert

--- starter-code/main.js
+++ starter-code/main.js
@@ -2,3 +2,8 @@ var cardOne = "queen";
 var cardTwo = "king";
 var cardThree = "queen";
 var cardFour = "king";
+if(cardOne === cardThree){
+    alert('You found a match!');
+} else{
+    alert('Sorry, try again.');
+};
